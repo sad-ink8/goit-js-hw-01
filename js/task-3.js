3524e7e0d0f13dc0406150fd7acd5bf9245e6459
Nelli <nelli.sedinkina.v@gmail.com>
2024-12-01 01:01:42 +0100

    Add code for checking

--- js/task-3.js
+++ js/task-3.js
@@ -5,3 +5,7 @@ function getElementWidth(content, padding, border) {
   const ElementWidth = c + p * 2 + b * 2;
   return ElementWidth;
 }
+
+console.log(getElementWidth('50px', '8px', '4px')); // 74
+console.log(getElementWidth('60px', '12px', '8.5px')); // 101
+console.log(getElementWidth('200px', '0px', '0px')); // 200
